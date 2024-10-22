cfad16d69dd53a153eab02d07d77ab3fc145493f
nurmuhammad <nurmuhammadhasanov0@gmail.com>
2024-10-22 08:29:47 +0500

    task complated

--- 1-dars/main.py
+++ 1-dars/main.py
@@ -2,7 +2,8 @@ destonation = int(input("Uyingizdan ishxonagacha necha km: "))
 UserMoney = int(input("Qancha pulingiz bor: "))
 TaxiPeerKM = int(input("Taxi 1 kM ga mechpul: "))
 BusPrice = int(input("Aftobus nechpul: "))
+busCount = int(input("Nechta aftobusda borasiz: "))
 
 TaxiPrice = destonation * TaxiPeerKM
 print(f"taxida Ketaolasizmi {TaxiPeerKM <= UserMoney}")
-print(f"Aftobusda ketaolasizmi {UserMoney >= BusPrice}")
+print(f"Aftobusda ketaolasizmi {UserMoney >= (BusPrice*busCount)}")
